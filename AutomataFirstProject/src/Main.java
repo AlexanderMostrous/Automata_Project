69bd12ba31d23f0e48b0554be24a5a4abc7471ac
alexandros <alexandros@Alexander-Desktop>
2017-10-20 13:00:28 +0300

    Trying out the .gitignore file.

--- AutomataFirstProject/src/Main.java
+++ AutomataFirstProject/src/Main.java
@@ -1,9 +1,16 @@
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 
+		
+		
+		String path = "C:\\Users\\alexandros\\Dropbox\\Χειμερινό\\Θεωρία Υπολογισμών και Αυτομάτων\\module.txt";
+		
+		new FormatTester(path);
 	}
 
 }
