9bfe2037db313fd2c48c91524f59703b2170f4e9
alexandros <alexandros@Alexander-Desktop>
2017-10-25 22:23:37 +0300

    Transition class created.
    This is a modeling change.

---
+++ AutomataFirstProject/src/Transition.java
@@ -0,0 +1,35 @@
+
+public class Transition {
+	
+	private State startingState, destinationState;
+	private String transitionSymbol;
+	
+	public Transition(State start, State destination, String symbol)
+	{
+		startingState = start;
+		destinationState = destination;
+		transitionSymbol = symbol;
+	}
+	
+	public boolean isANullTransition()
+	{
+		if(transitionSymbol.equals("å"))
+			return true;
+		else
+			return false;
+	}
+
+	public State getStartingState() {
+		return startingState;
+	}
+
+	public State getDestinationState() {
+		return destinationState;
+	}
+
+	public String getTransitionSymbol() {
+		return transitionSymbol;
+	}
+	
+	
+}
